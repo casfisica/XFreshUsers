80ff54cd7cd83af4ed50b98f2e9e3ae223a15f15
camilo salazar <camilo.salazar@udea.edu.co>
2022-03-02 17:11:46 -0500

    First Commit

---
+++ XFreshUsers.Android/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+using XFreshUsers.Services;
+using FreshMvvm;
+
+namespace XFreshUsers.Droid
+{
+    [Activity(Label = "XFreshUsers", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            //Inject the repository instance on Android
+            var repository = new Repository(FileAccessHelper.GetLocalFilePath("items.db3"));
+            FreshIOC.Container.Register(repository);
+
+            LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ XFreshUsers/PageModels/UserPageModel.cs
@@ -0,0 +1,90 @@
+﻿using FreshMvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+using XFreshUsers.Models;
+using XFreshUsers.Services;
+
+namespace XFreshUsers.PageModels
+{
+    public class UserPageModel : FreshBasePageModel
+    {
+        // Use IoC to get our repository.
+        private Repository _repository = FreshIOC.Container.Resolve<Repository>();
+
+        // Backing data model.
+        private User _user;
+
+        /// <summary>
+        /// Public property exposing the user's name for Page binding.
+        /// </summary>
+        public string UserName
+        {
+            get { return _user.Name; }
+            set { _user.Name = value; RaisePropertyChanged(); }
+        }
+
+        /// <summary>
+        /// Public property exposing the user's barcode for Page binding.
+        /// </summary>
+        public string UserPassWord
+        {
+            get { return _user.PassWord; }
+            set { _user.PassWord = value; RaisePropertyChanged(); }
+        }
+
+        /// <summary>
+        /// Public property exposing the user's quantity for Page binding.
+        /// </summary>
+        public int UserIsAdmin
+        {
+            get { return _user.IsAdmin; }
+            set 
+            {
+                if (value==1)
+                {
+                    _user.IsAdmin = 1; RaisePropertyChanged();
+                }
+                else 
+                { 
+                    _user.IsAdmin = 0; RaisePropertyChanged(); 
+                }
+             }
+        }
+
+        /// <summary>
+        /// Called whenever the page is navigated to.
+        /// Either use a supplied User, or create a new one if not supplied.
+        /// FreshMVVM does not provide a RaiseAllPropertyChanged,
+        /// so we do this for each bound property, room for improvement.
+        /// </summary>
+        public override void Init(object initData)
+        {
+            _user = initData as User;
+            if (_user == null) _user = new User();
+            base.Init(initData);
+            RaisePropertyChanged(nameof(UserName));
+            RaisePropertyChanged(nameof(UserPassWord));
+        }
+
+        /// <summary>
+        /// Command associated with the save action.
+        /// Persists the user to the database if the user is valid.
+        /// </summary>
+        public ICommand SaveCommand
+        {
+            get
+            {
+                return new Command(async () => {
+                    if (_user.IsValid())
+                    {
+                        await _repository.CreateUser(_user);
+                        await CoreMethods.PopPageModel(_user);
+                    }
+                });
+            }
+        }
+    }
+}
